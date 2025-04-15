c40f1bf54acd1647f80306954f18ffe77788d843
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-04-15 12:28:15 +0330

    first commit

---
+++ src/js/table.js
@@ -0,0 +1,11 @@
+window.addEventListener('load',function () {
+  const table= document.querySelector('.editor-section').querySelector('table');
+  const newDiv = document.createElement("div");
+  table.parentNode.insertBefore(newDiv,table);
+  newDiv.appendChild(table);
+  newDiv.classList.add('overflow-x-auto');
+  newDiv.classList.add('mt-11');
+  newDiv.classList.add('shadow-13');
+  newDiv.classList.add('rounded-[24px]');
+  newDiv.classList.add('px-6');
+})
