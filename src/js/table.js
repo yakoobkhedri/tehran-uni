de447c2bc616529392e8c5a6169d7278d7103eab
yakoobkhedri <yakkobkhedrii@gmail.com>
2025-04-15 21:46:47 +0330

    first commit

--- src/js/table.js
+++ src/js/table.js
@@ -4,7 +4,8 @@ window.addEventListener('load',function () {
   table.parentNode.insertBefore(newDiv,table);
   newDiv.appendChild(table);
   newDiv.classList.add('overflow-x-auto');
-  newDiv.classList.add('mt-11');
+  newDiv.classList.add('sm:mt-11');
+  newDiv.classList.add('mt-4');
   newDiv.classList.add('shadow-13');
   newDiv.classList.add('rounded-[24px]');
   newDiv.classList.add('px-6');
